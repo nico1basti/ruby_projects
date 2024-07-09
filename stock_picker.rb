f8525a148afc14bb71b611d38148f02a90968e90
nico1basti <fnicolasbastidas7@gmail.com>
2024-07-09 13:55:11 -0500

    Optimize stock_picker.rb

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,22 +2,20 @@
 stock_prices = [17,3,6,9,15,8,6,1,10]
 
 def stock_picker (prices)
-  sorted_prices = prices.sort
   max_revenue = {
     revenue: 0,
     min_price_index: 0,
     max_price_index: 0
   }
-  sorted_prices.each {|price|
-    price_index = prices.find_index(price)
-    max_price = prices[price_index+1 ..].max || price 
+  prices.each.with_index {|price,p_index|
+    max_price = prices[p_index+1 ..].max || price 
+    profit = max_price - price
     max_price_index = prices.find_index(max_price)
-    revenue = max_price - price
 
-    if (max_price - price) > max_revenue[:revenue] 
+    if profit > max_revenue[:revenue] 
       max_revenue = {
-        revenue: revenue,
-        min_price_index: price_index,
+        revenue: profit,
+        min_price_index: p_index,
         max_price_index: max_price_index
       }
     end
